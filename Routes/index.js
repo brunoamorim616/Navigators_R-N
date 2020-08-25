7c50a3d2f31301571f6fce9c22f81f2ac0e7e780
brunoamorim616 <bruno_amorim1@estudante.sc.senai.br>
2020-08-25 20:52:51 -0300

    bottom tab e rota editada

--- Routes/index.js
+++ Routes/index.js
@@ -3,8 +3,7 @@
 import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {NavigationContainer} from '@react-navigation/native';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, Entypo } from '@expo/vector-icons';
 
 
 import { Home } from '../Components/Home/index';
@@ -16,29 +15,61 @@ const RootStack = createStackNavigator();
 
 export const RootStackScreen = () =>(
 
-  <RootStack.Navigator>
-    <RootStack.Screen name="App" component={BottomTabScreen} />
+  <RootStack.Navigator headerMode="none">
+    <RootStack.Screen name="App" component={BottomTabStack} />
   </RootStack.Navigator>
 
 );
 
 const BottomTab = createBottomTabNavigator();
 
-const BottomTabScreen = () => (
-
-  <BottomTab.Navigator>
-    <BottomTab.Screen name="Home" component={Home} />
-    <BottomTab.Screen name="Showcase" component={ShowcaseScreen} />
-    <BottomTab.Screen name="Settings" component={Settings} />
+const BottomTabStack = () => (
+  
+  <BottomTab.Navigator tabBarOptions={{
+    inactiveBackgroundColor: 'black',
+    inactiveTintColor: 'white',
+    activeBackgroundColor: 'white',
+    activeTintColor:'black',    
+}}>
+    <BottomTab.Screen name="Home" component={HomeStack} options={{
+      tabBarLabel: 'Princial',
+      tabBarIcon: ({color}) => (
+        <Ionicons name="md-home" color={color} size={30} />
+      ),
+      }}/>
+    <BottomTab.Screen name="Showcase" component={ShowcaseStack} options={{
+      tabBarLabel: 'Vitrine',
+      tabBarIcon: ({color}) => (
+        <Entypo name="shop" color={color} size={30} />
+      ),
+      }}/>
+    <BottomTab.Screen name="Settings" component={SettingsStack} options={{
+      tabBarLabel: 'Configurações',
+      tabBarIcon: ({color}) => (
+        <Ionicons name="md-settings" color={color} size={30} />
+      ),
+      }}/>
   </BottomTab.Navigator>
 
 );
 
 const Stack = createStackNavigator();
 
-const ShowcaseScreen = () => (
-  <Stack.Navigator>
+const ShowcaseStack = () => (
+  <Stack.Navigator headerMode="screen">
     <Stack.Screen name="Showcase" component={Showcase} />
     <Stack.Screen name="ShowcaseItem" component={ShowcaseItem} />
   </Stack.Navigator>
 );
+
+const HomeStack = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Home" component={Home}/>
+  </Stack.Navigator>
+);
+
+const SettingsStack = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Settings" component={Settings}/>
+  </Stack.Navigator>
+)
